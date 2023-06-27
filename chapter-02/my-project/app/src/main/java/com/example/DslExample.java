0782accbb76e500b3e4fc3f911366801cc2177ac
pt7481 <Paul.tongyoo@gmail.com>
2023-06-26 17:38:07 -0700

    Removed unnecessary lines from build.gradle; Added Properties config object

--- chapter-02/my-project/app/src/main/java/com/example/DslExample.java
+++ chapter-02/my-project/app/src/main/java/com/example/DslExample.java
@@ -2,8 +2,11 @@ package com.example;
 
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 
 public class DslExample {
@@ -18,6 +21,11 @@ public class DslExample {
             });
 
         Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "dev1");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Void().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         KafkaStreams streams = new KafkaStreams(builder.build(), config);
         streams.start();
